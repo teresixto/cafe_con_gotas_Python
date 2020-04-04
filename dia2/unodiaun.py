d37be94e665930040e272f3be9eeaec8d3b3680a
Teresa Sixto Anello <tere.sixto@gmail.com>
2020-04-04 13:34:22 +0200

    Fichero creados del dia dos

---
+++ dia2/unodiaun.py
@@ -0,0 +1,23 @@
+''' todo esto é un comentario
+multilinea
+jejejeje
+'''
+#kkgjfslkj
+#lkfjg
+#lskgjkjs
+
+nome = 'Guido' #esto e un comentario
+print ('Hola' + + nome) #agora imprimo
+
+if (nome == 'salvari'):
+    print ('cambiou de nome')
+else ('seguramente o nome é Guido')
+
+if nome == 'salvari':
+    print (1)
+elif (nome == 'Guido'):
+    print (2)
+elif (3):
+    print (3)
+else:
+    print(0)
